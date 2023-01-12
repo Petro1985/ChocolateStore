949ab7c0fb89d1c8bc2476beaae0cd3dae749ead
Petr <Petro1985p@gmail.com>
2023-01-12 21:16:15 +0300

    Don't forget about migrations

---
+++ ChocolateData/Migrations/20230112165405_ImageIntoDB.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChocolateData.Migrations
+{
+    public partial class ImageIntoDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PathToPhoto",
+                table: "Photos");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "MainPhotoId",
+                table: "Products",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "Photos",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_MainPhotoId",
+                table: "Products",
+                column: "MainPhotoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Photos_MainPhotoId",
+                table: "Products",
+                column: "MainPhotoId",
+                principalTable: "Photos",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Photos_MainPhotoId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_MainPhotoId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "MainPhotoId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Photos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PathToPhoto",
+                table: "Photos",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
