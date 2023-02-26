3b043bc645d637ad798c38b6fb8680ad42d2a7f4
Petr <Petro1985p@gmail.com>
2023-02-26 18:01:20 +0300

    Added delete confirmation. Improved navbar

--- ChocolateUI/Pages/DisplayCategories/CategoriesBase.cs
+++ ChocolateUI/Pages/DisplayCategories/CategoriesBase.cs
@@ -10,7 +10,6 @@ public class CategoryBase : ComponentBase
     [Inject] public IFetchService FetchServ { get; set; }
     [Inject] public IUserProfile UserProfile { get; set; }
     [Inject] public ILogger<CategoryBase> Logger { get; set; }
-    [Inject] public CategoryState State { get; set; }
 
     public Dictionary<Guid, CategoryDTO>? Categories { get; set; }
 
@@ -24,6 +23,5 @@ public class CategoryBase : ComponentBase
     {
         var categoriesCollection = await FetchServ.GetCategories();
         Categories = categoriesCollection.ToDictionary(x => x.Id);
-        State.Categories = Categories;
     }
 }
--- ChocolateUI/Services/CategoryItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChocolateUI.Services;
-
-public class CategoryItem : IItem
-{
-    public Guid Id { get; init; }
-    public string Name { get; set; }
-    public Guid MainPhotoId { get; set; }
-}
