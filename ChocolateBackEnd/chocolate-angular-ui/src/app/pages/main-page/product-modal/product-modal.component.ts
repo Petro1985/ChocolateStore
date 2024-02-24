cb389c157ab0ce53639966b71ea7c51f6bfb1438
Прозоров Петр <prozorov_p@ews.ru>
2024-02-24 18:50:59 +0300

    Слайдер, наконец-то, работает!

--- ChocolateBackEnd/chocolate-angular-ui/src/app/pages/main-page/product-modal/product-modal.component.ts
+++ ChocolateBackEnd/chocolate-angular-ui/src/app/pages/main-page/product-modal/product-modal.component.ts
@@ -2,6 +2,7 @@ import {Component, ElementRef, Input, OnInit, Renderer2, SimpleChanges, ViewChil
 import {IProduct} from "../../../services/contracts/products";
 import {ImageService} from "../../../services/imageService";
 import {SwiperContainer} from "swiper/swiper-element";
+import {Swiper} from "swiper";
 // import {SwiperOptions} from "swiper/types";
 // import { A11y, Mousewheel, Navigation, Pagination, Thumbs} from "swiper/types/modules";
 // import { Thumbs } from "swiper/types/modules";
@@ -15,54 +16,28 @@ export class ProductModalComponent implements OnInit {
   @Input() product!: IProduct;
   @Input() closeFunction!: Function;
 
-  // @ViewChild('mainSwiper', {static: true}) mainSwiper!: ElementRef<SwiperContainer>
-  // @ViewChild('thumbnailSwiper', {static: true}) thumbnailSwiper!: ElementRef<SwiperContainer>
-  // @ViewChild('testSwiper', {static: true}) testSwiper!: ElementRef<SwiperContainer>
-  @ViewChild('mainSwiper', {static: false}) mainSwiper!: ElementRef<SwiperContainer>
-  @ViewChild('thumbnailSwiper', {static: false}) thumbnailSwiper!: ElementRef<SwiperContainer>
-  @ViewChild('testSwiper', {static: false}) testSwiper!: ElementRef<SwiperContainer>
+  @ViewChild('mainSwiper', {static: true}) mainSwiper!: ElementRef<SwiperContainer>
+  @ViewChild('thumbnailSwiper', {static: true}) thumbnailSwiper!: ElementRef<SwiperContainer>
+  @ViewChild('testSwiper', {static: true}) testSwiper!: ElementRef<SwiperContainer>
 
-  currentPhoto: number = 0;
-
-  constructor(public imageService: ImageService, private renderer: Renderer2) {
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    // if (changes['product']) {
-    //   if (this.mainSwiper) {
-    //     // Заполняем основной Swiper
-    //     this.mainSwiper.nativeElement.swiper.virtual.removeAllSlides();
-    //     this.mainSwiper.nativeElement.swiper.virtual.appendSlide(this.getMainSlides());
-    //     this.mainSwiper.nativeElement.swiper.virtual.update(true);
-    //     // this.mainSwiper.nativeElement.swiper.removeAllSlides();
-    //     // this.mainSwiper.nativeElement.swiper.appendSlide(this.getMainSlides());
-    //     // this.mainSwiper.nativeElement.swiper.appendSlide(this.getMainSlides());
-    //     // this.mainSwiper.nativeElement.slidesPerView = 1;
-    //     // this.mainSwiper.nativeElement.swiper.update();
-    //     this.mainSwiper.nativeElement.swiper.activeIndex = 0;
-    //
-    //     // this.mainSwiper.nativeElement.loop = this.mainSwiper.nativeElement.swiper.virtual.slides.length > 2;
-    //
-    //     // Заполняем Swiper для маленьких фотографий
-    //     this.thumbnailSwiper.nativeElement.swiper.virtual.removeAllSlides();
-    //     this.thumbnailSwiper.nativeElement.swiper.virtual.appendSlide(this.getThumbnailSlides());
-    //     this.thumbnailSwiper.nativeElement.swiper.virtual.update(false);
-    //     this.thumbnailSwiper.nativeElement.swiper.activeIndex = 0;
-    //     this.thumbnailSwiper.nativeElement.loop = this.thumbnailSwiper.nativeElement.swiper.virtual.slides.length > 2;
-    //   }
-    // }
+  constructor(public imageService: ImageService) {
   }
 
   onThumbnailClick(i:number, e: any): void {
-    // this.mainSwiper.nativeElement.swiper.slideNext();
     this.mainSwiper.nativeElement.swiper.slideTo(i, 500);
+  }
+
+  private setActiveThumbnail(newActiveNumber: number)
+  {
     this.thumbnailSwiper.nativeElement.swiper.slides.forEach(x => {
       const divContainer = x.querySelector('div.swiper-thumbnail-slide-image');
       if (divContainer){
         divContainer.classList.remove('thumb-slide-active');
       }
     })
-    e.target.parentElement.classList.add('thumb-slide-active');
+
+    this.thumbnailSwiper.nativeElement.swiper.slides[newActiveNumber]
+      .querySelector('div.swiper-thumbnail-slide-image')?.classList.add('thumb-slide-active');
   }
 
   Close() {
@@ -71,80 +46,21 @@ export class ProductModalComponent implements OnInit {
   }
 
   ngOnInit() {
-    // const config: SwiperOptions = {
-    //   // modules: [ Navigation, Pagination, A11y, Mousewheel],
-    //   autoHeight: true,
-    //   spaceBetween: 20,
-    //   navigation: false,
-    //   pagination: {clickable: true, dynamicBullets: true},
-    //   slidesPerView: 1,
-    //   centeredSlides: true,
-    //   breakpoints: {
-    //     400: {
-    //       slidesPerView: "auto",
-    //       centeredSlides: false
-    //     },
-    //   }
-    // }
+    this.mainSwiper.nativeElement.swiper.on('slideChange' ,swiper => this.MainSwiperOnSlideChange(swiper));
+    this.mainSwiper.nativeElement.swiper.on('activeIndexChange' ,swiper => this.setActiveThumbnail(swiper.realIndex));
+    this.mainSwiper.nativeElement.swiper.on('slidesUpdated' ,swiper => this.onSwiperInit(swiper));
 
-    // this.mainSwiper.nativeElement.slidesPerView = 1;
-    // this.mainSwiper.nativeElement.speed= 500;
-    // this.mainSwiper.nativeElement.loop = false;
-    //
-    // this.thumbnailSwiper.nativeElement.spaceBetween = 2;
-    // this.thumbnailSwiper.nativeElement.slidesPerView = 4;
-    // this.thumbnailSwiper.nativeElement.swiper.params.freeMode = true;
   }
 
-  // private getThumbnailSlides() {
-  //   return this.product.photos.map((x, i) => {
-  //     const imageContainerHtmlElement: HTMLDivElement = this.renderer.createElement('div');
-  //     const imageHtmlElement: HTMLImageElement = this.renderer.createElement('img');
-  //
-  //     imageHtmlElement.src = this.imageService.getThumbnailImageUrl(x);
-  //     imageHtmlElement.style.width = '100%';
-  //     imageHtmlElement.style.height = '100%';
-  //
-  //     imageContainerHtmlElement.classList.add('swiper-thumbnail-slide-image');
-  //
-  //     imageHtmlElement.addEventListener('click', (e) => this.onThumbnailClick(i, e))
-  //
-  //     // if (i === 0)
-  //     // {
-  //     //   slideHtmlElement.classList.add('swiper-thumbnail-slide-image');
-  //     // }
-  //
-  //     imageContainerHtmlElement.appendChild(imageHtmlElement);
-  //     return imageContainerHtmlElement.outerHTML;
-  //   });
-  // }
-  //
-  // private getMainSlides() {
-  //   return this.product.photos?.map((x, i) => {
-  //     const imageContainerHtmlElement: HTMLDivElement = this.renderer.createElement('div');
-  //     const imageHtmlElement: HTMLImageElement = this.renderer.createElement('img');
-  //
-  //     imageHtmlElement.src = this.imageService.getImageUrl(x);
-  //     imageHtmlElement.style.width = '100%';
-  //     imageHtmlElement.style.height = '100%';
-  //
-  //     // imageContainerHtmlElement.classList.add('swiper-slide-image');
-  //
-  //     // if (i === 0)
-  //     // {
-  //     //   slideHtmlElement.classList.add('swiper-thumbnail-slide-image');
-  //     // }
-  //
-  //     imageContainerHtmlElement.appendChild(imageHtmlElement);
-  //     return imageContainerHtmlElement.outerHTML;
-  //   }) ?? [];
-  // }
-  OnMainSwiperUpdate() {
-    if (this.product.photos)
+  MainSwiperOnSlideChange(swiper: Swiper) {
+    if (this.product?.photos)
     {
-      this.mainSwiper.nativeElement.loop = this.product.photos.length > 2;
-      this.mainSwiper.nativeElement.swiper.slideTo(0, 0);
-      console.log('updated!!!')
+      this.thumbnailSwiper.nativeElement.swiper.slideTo(swiper.realIndex, 500);
     }
+  }
+
+  private onSwiperInit(swiper: Swiper) {
+    this.thumbnailSwiper.nativeElement.swiper.slideTo(0, 0);
+    this.mainSwiper.nativeElement.swiper.slideTo(0, 0);
   }
 }
